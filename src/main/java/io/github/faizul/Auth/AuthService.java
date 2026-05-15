7ca74ae742873b7fa143dde656059efdd47b7d18
faizulmushofa <faizulmushofa1@gmail.com>
2026-05-16 06:53:45 +0700

    fixing bug register

--- src/main/java/io/github/faizul/Auth/AuthService.java
+++ src/main/java/io/github/faizul/Auth/AuthService.java
@@ -3,6 +3,8 @@ package io.github.faizul.Auth;
 import io.github.faizul.Auth.Dtos.*;
 import io.github.faizul.Jwt.JwtService;
 import io.github.faizul.User.UserRepository;
+import io.github.faizul.User.UserService;
+import io.github.faizul.UserRole.UserRoleService;
 import io.github.faizul.Utils.Mappings.AuthMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -16,10 +18,11 @@ public class AuthService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+    private final UserService userService;
 
     public Mono<RegisterResponse> register(RegisterRequest request){
 
-        return userRepository.save(AuthMapper.toUser(request, passwordEncoder))
+        return userService.createUser(AuthMapper.toUser(request))
                 .thenReturn(new RegisterResponse("Register Successfully"));
 
     }
--- src/main/java/io/github/faizul/Utils/Mappings/AuthMapper.java
+++ src/main/java/io/github/faizul/Utils/Mappings/AuthMapper.java
@@ -10,11 +10,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class AuthMapper {
 
-    public static User toUser(RegisterRequest request, PasswordEncoder passwordEncoder){
+    public static User toUser(RegisterRequest request){
         return User.builder()
                 .email(request.email())
                 .username(request.username())
-                .password(passwordEncoder.encode(request.password()))
+                .password(request.password())
                 .build();
     }
 }
