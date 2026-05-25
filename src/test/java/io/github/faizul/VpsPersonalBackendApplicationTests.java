770ee0e3184109e0ceb79be27bdb7d828fb31efe
faizulmushofa <faizulmushofa1@gmail.com>
2026-05-25 23:42:05 +0700

    add : verification key

--- src/test/java/io/github/faizul/VpsPersonalBackendApplicationTests.java
+++ src/test/java/io/github/faizul/VpsPersonalBackendApplicationTests.java
@@ -3,8 +3,14 @@ package io.github.faizul;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import com.google.genai.Client;
+
 @SpringBootTest
 class VpsPersonalBackendApplicationTests {
+
+    @MockitoBean
+    private Client geminiClient;
 
     @Test
     void contextLoads() {
